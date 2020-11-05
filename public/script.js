f2b9c473b76ece69a4b4680fbff0a6cdb84e6771
hong2tu <58047668+hong2tu@users.noreply.github.com>
2020-11-05 14:03:31 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,9 @@
+const { Script } = require("vm");
 
+<script>
+const enpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
+const food = [];
+
+
+fetch(endpoint).then()
+</script>
