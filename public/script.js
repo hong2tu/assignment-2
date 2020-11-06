4af86fbe5104b43a6ae0a598234baf0445066bd6
samanthabarlage <70207255+samanthabarlage@users.noreply.github.com>
2020-11-05 21:51:21 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,9 +1,38 @@
-const { Script } = require("vm");
-
-<script>
-const enpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
+//const { Script } = require("vm");
+const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
 const food = [];
 
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => food.push(...data));
 
-fetch(endpoint).then()
-</script>
+function findMatches(wordToMatch, food) {
+  return food.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.name.match(regex) 
+  });
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, food);
+    const html = matchArray.map(place => {
+      const regex = new RegExp(this.value, 'gi');
+      const restaurantName = place.name.replace(regex, `<span class="hl">${this.value}</span>`);
+      return `
+        <li>
+          <span class="name">${restaurantName}</span>
+        </li>
+      `;
+    }).join('');
+    suggestions.innerHTML = html;
+  }
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
